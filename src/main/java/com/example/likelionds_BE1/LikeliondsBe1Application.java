ade775e2f70ff4804673630bee4c5ca1657d417d
yslee <yeongseo6142@naver.com>
2025-09-30 00:28:58 +0900

    환경세팅

---
+++ src/main/java/com/example/likelionds_BE1/LikeliondsBe1Application.java
@@ -0,0 +1,13 @@
+package com.example.likelionds_BE1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeliondsBe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeliondsBe1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/likelionds_BE1/LikeliondsBe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionds_BE1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeliondsBe1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
